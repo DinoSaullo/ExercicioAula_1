fdd46e264d971376425cd91257eb59be071f7c6d
Saulo <saulo.benedetti@hotmail.com>
2023-03-30 21:10:58 -0300

    Realizado atividade sobre datas

--- src/projeto/git/exercicio/aula/Exercicio.java
+++ src/projeto/git/exercicio/aula/Exercicio.java
@@ -1,8 +1,21 @@
 package projeto.git.exercicio.aula;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class Exercicio {
 
 	public static void main(String[] args) {
+
+		DateFormat formatoFull = DateFormat.getDateInstance(DateFormat.FULL);
+
+		Date data = new Date();
+
+		int horas = data.getHours();
+		int minutos = data.getMinutes();
+
+		System.out.println("Hoje é " + formatoFull.format(data).replaceAll(",", "") + " e agora são " + horas
+				+ " horas e " + minutos + " minutos.");
 
 	}
 
